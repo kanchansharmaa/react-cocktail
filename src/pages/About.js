5e655c7576673b8a1ca1192dd44d12e166992b30
kanchansharmaa <kanchan2175sharma@gmail.com>
2022-12-19 08:01:44 +0530

    intial commit

---
+++ src/pages/About.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <section className='section about-section'>
+      <h1 className='section-title'> about us</h1>
+    <p>
+    Welcome to TheCocktailDB An open, crowd-sourced database of drinks and cocktails from around the world.
+We also offer a free JSON API for anyone wanting to use it.
+You can use the test API key "1" during development of your app or for educational use
+However you must sign up to Patreon if you want a production API key if releasing publicly on an appstore.
+    </p>
+    
+    </section>
+  )
+}
+
+export default About;
